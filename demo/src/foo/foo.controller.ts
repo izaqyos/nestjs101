d4665011c024058c6250f4aac46d92f212598411
i500695 <yosi.izaq@sap.com>
2020-10-15 17:56:17 +0300

    add CRUD. get/delete/update by ID.

--- demo/src/foo/foo.controller.ts
+++ demo/src/foo/foo.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateFooDTO } from './dto/create-foo.dto';
-import { Foo } from './foo.model';
+import { Foo, FooState } from './foo.model';
 import { FooService } from './foo.service';
 
 @Controller('foo')
@@ -13,6 +13,23 @@ export class FooController {
     getFoos(): Foo[] {
         return this.fooService.getFoos();
     }
+
+    @Get(':id')
+    getById(@Param('id') id: string): Foo {
+        return this.fooService.getById(id);
+    }
+
+    @Delete(':id')
+    delById(@Param('id') id: string): void {
+        this.fooService.delById(id);
+    }
+
+    @Patch(':id/state')
+    updateState(@Param('id') id: string, @Body('state') state: FooState): Foo {
+        console.log('got request to update state of %s to %s', id, state);
+        return this.fooService.updateState(id, state);
+    }
+
 
     @Post()
     createFoo(@Body() createFooDto: CreateFooDTO) {
